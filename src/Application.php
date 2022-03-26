ad1612ac0ad1ca01ef1030bdec64d20d39a785b9
spiwowar <spiwowar@trans.eu>
2022-03-26 23:48:16 +0100

    custom error page

--- src/Application.php
+++ src/Application.php
@@ -9,14 +9,20 @@ use DI\Definition\Source\DefinitionFile;
 use DI\Definition\Source\ReflectionBasedAutowiring;
 use Dotenv\Dotenv;
 use KanbanBoard\Application\Controller\IndexController;
+use Mustache_Engine;
+use Mustache_Loader_FilesystemLoader;
 use RuntimeException;
+use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 class Application
 {
     protected const SERVICES_FILE = __DIR__ . '/../config/services.php';
     protected const ENV_DIR = __DIR__ . '/../';
 
-    protected Container $container;
+    private Container $container;
+    private Mustache_Engine $viewEngine;
+    private Response $response;
 
     public static function init(): self
     {
@@ -25,8 +31,16 @@ class Application
         $builder = new ContainerBuilder();
         $builder->useAutowiring(true);
         $builder->addDefinitions(new DefinitionFile(self::SERVICES_FILE, new ReflectionBasedAutowiring()));
+        $container = $builder->build();
 
-        return new self($builder->build());
+        // TODO: use factory to make it replaceable
+        $viewEngine = new Mustache_Engine([
+            'pragmas' => [Mustache_Engine::PRAGMA_BLOCKS],
+            'loader' => new Mustache_Loader_FilesystemLoader('../src/views'),
+            'cache' => '../var/cache',
+        ]);
+
+        return new self($container, $container->get(Response::class), $viewEngine);
     }
 
     public function run(
@@ -45,11 +59,39 @@ class Application
             );
         }
 
-        $this->container->get($controller)->$action();
+        $template = static function (string $controller, string $action): string {
+            $explodedController = explode('\\', $controller);
+
+            return sprintf('%s/%s',
+                str_replace('controller', '', strtolower(array_pop($explodedController))),
+                $action,
+            );
+        };
+
+        try {
+            $context = $this->container->get($controller)->$action();
+            $templateName = $template($controller, $action);
+
+            if (!is_array($context)) {
+                throw new RuntimeException('Returning array data in controller is required');
+            }
+        } catch (Throwable $e) {
+            $templateName = 'error/error';
+            $context = [
+                'message' => $e->getMessage(),
+                'code' => $e->getCode(),
+                'exception' => $e
+            ];
+        } finally {
+            $this->response->setContent($this->viewEngine->render($templateName, $context));
+            $this->response->send();
+        }
     }
 
-    private function __construct(Container $container)
+    private function __construct(Container $container, Response $response, Mustache_Engine $viewEngine)
     {
         $this->container = $container;
+        $this->viewEngine = $viewEngine;
+        $this->response = $response;
     }
 }
--- src/Application/Controller/IndexController.php
+++ src/Application/Controller/IndexController.php
@@ -9,8 +9,6 @@ use KanbanBoard\Application\Provider\TokenProvider\TokenProviderInterface;
 use KanbanBoard\Application\Repository\BoardRepositoryInterface;
 use KanbanBoard\Domain\Issue;
 use KanbanBoard\Domain\Milestone;
-use Mustache_Engine;
-use Mustache_Loader_FilesystemLoader;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -18,24 +16,21 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class IndexController
 {
-    private Response $response;
     private TokenProviderInterface $tokenProvider;
     private BoardRepositoryInterface $boardRepository;
     private RepositoriesProviderInterface $repositoriesProvider;
 
     public function __construct(
-        Response $response,
         TokenProviderInterface $tokenProvider,
         BoardRepositoryInterface $boardRepository,
         RepositoriesProviderInterface $repositoriesProvider
     ) {
-        $this->response = $response;
         $this->tokenProvider = $tokenProvider;
         $this->boardRepository = $boardRepository;
         $this->repositoriesProvider = $repositoriesProvider;
     }
 
-    public function index(): void
+    public function index(): array
     {
         $token = $this->tokenProvider->provide();
 
@@ -68,21 +63,8 @@ class IndexController
             ], $this->boardRepository->getByRepository($token, $repository)->getMilestones())
         ], $this->repositoriesProvider->provide());
 
-        $this->render('index/index', [
+        return [
             'milestones' => array_merge(...array_merge(...$milestones)),
-        ]);
-    }
-
-    protected function render(string $template, array $context = []): void
-    {
-        // TODO: inject view engine
-        $viewEngine = new Mustache_Engine([
-            'pragmas' => [Mustache_Engine::PRAGMA_BLOCKS],
-            'loader' => new Mustache_Loader_FilesystemLoader('../src/views'),
-            'cache' => '../var/cache',
-        ]);
-
-        $this->response->setContent($viewEngine->render($template, $context));
-        $this->response->send();
+        ];
     }
 }
